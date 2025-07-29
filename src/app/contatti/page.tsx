7566d5548490e875c7ed6914b33d6d5b4e48e9e2
Stefano Bisonni <bisonni.stefano@gmail.com>
2025-07-29 22:31:46 +0200

    Addedd address

--- src/app/contatti/page.tsx
+++ src/app/contatti/page.tsx
@@ -12,21 +12,39 @@ export default function ContattiPage() {
           Ogni azienda ha esigenze specifiche. Per questo motivo ogni consulenza è progettata su misura.
         </p>
 
-        {/* Mail di contatto */}
-        <p className="text-2xl text-gray-800 mb-3">
-          <a href="mailto:sonia@soniapaglialunga.it" className="font-semibold underline underline-offset-4 decoration-sky-300 hover:text-orange-300 transition">
-            sonia@soniapaglialunga.it
-          </a>
-        </p>
 
-        {/* Frase finale */}
-        <p className="text-base text-gray-500 italic mb-20">
-          Una consulenza mirata può fare la differenza tra una buona impressione e una presenza memorabile.
-        </p>
+        <div className="flex flex-col gap-6 justify-center ">
 
-        {/* Paragrafo introduttivo */}
+          <span>
+            <p className="text-sm text-gray-500 italic ">
+              Per qualunque dubbio o curiosità, non esitate a contattarmi
+            </p>
+            <p className="text-2xl text-gray-800 mb-5">
+              <a href="mailto:sonia@soniapaglialunga.it" className="font-semibold underline underline-offset-4 decoration-sky-300 hover:text-orange-300 transition">
+                sonia@soniapaglialunga.it
+              </a>
+            </p>
+          </span>
+          <p className="text-sm text-gray-500 italic">
+              Oppure raggiungici direttamente in sede, previo appuntamento
+            </p>
+          <span>
+            <p className="text-2xl text-gray-800 font-semibold">
+              Via Timavo, 4
+            </p>
+            <p className="text-xl text-gray-500 font-semibold">
+              62029 Tolentino (MC)
+            </p>
+
+            <p className="text-sm text-gray-500 italic mb-20">
+              Una consulenza mirata può fare la differenza tra una buona impressione e una presenza memorabile.
+            </p>
+          </span>
+        </div>
+
+
         <p className="max-w-3xl mx-auto text-lg text-gray-700 mb-4">
-          Se desideri valorizzare l&apos;immagine del tuo team, potenziare la presenza dei tuoi leader 
+          Se desideri valorizzare l&apos;immagine del tuo team, potenziare la presenza dei tuoi leader
           o migliorare la percezione del brand attraverso una comunicazione visiva efficace, sono qui per affiancarti.
         </p>
 
